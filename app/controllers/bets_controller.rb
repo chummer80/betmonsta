434a5e5b849d21a3c7a12f6fc0cfb682ddd4b21e
chummer80 <mike.c.chu@gmail.com>
2015-06-03 22:53:34 -0700

    Allow bets to be placed and saved in the database

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -23,8 +23,45 @@ class BetsController < ApplicationController
 
 	def create
 		sport = params[:sport]
-		lines = params[:lines]
-		boxes = params[:boxes].values
+		lines = JSON.parse(params[:lines])
+		boxes = params[:boxes]
+		bet_amount = params[:bet_amount].to_f
+
+		if boxes
+			if bet_amount > 0
+				boxes.each do |match_index, match_boxes|
+					line = lines[match_index.to_i]
+					match_boxes.each_key do |home_away| 
+						bet_params = {
+							match_time: line["timestamp"],
+							sport: sport,
+							home_team: line["teams"]["home"],
+							away_team: line["teams"]["away"],
+							home_picked: home_away == "home",
+							spread: line["spread"][home_away],
+							odds: line["odds"][home_away],
+							risk_amount: bet_amount
+						}
+
+						new_bet = Bet.new(bet_params)
+						if new_bet.valid?
+							current_user.bets.push(new_bet)
+							flash[:info] = "Your bets have been placed!"
+						else
+							error_msg = "Some of your bets could not be placed"
+						end
+					end
+				end
+			else
+				error_msg = "You must wager some money"
+			end
+		else
+			error_msg = "You must select at least one bet"
+		end
+
+		if error_msg
+			flash[:alert] = error_msg
+		end
 
 		redirect_to new_bets_path(sport)
 	end
--- app/models/bet.rb
+++ app/models/bet.rb
@@ -5,7 +5,7 @@ class Bet < ActiveRecord::Base
   validates :sport, presence: true
   validates :home_team, presence: true
   validates :away_team, presence: true
-  validates :home_picked, presence: true
+  validates :home_picked, inclusion: { in: [true, false] } 
   validates :spread, presence: true
   validates :odds, presence: true
   validates :risk_amount, presence: true, numericality: {greater_than: 0}
