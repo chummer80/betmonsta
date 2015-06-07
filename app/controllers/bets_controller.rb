d2c4d91a3a9f85c76f0865c602e4f3751e961338
chummer80 <mike.c.chu@gmail.com>
2015-06-07 10:26:49 -0700

    Subtract funds from player account when bets are placed.  Don't allow any bets to be placed if user tries to bet too much at once.

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -32,33 +32,55 @@ class BetsController < ApplicationController
 		lines = JSON.parse(params[:lines])
 		boxes = params[:boxes]
 		bet_amount = params[:bet_amount].to_f
+		placed_bet_count = 0
 
 		if boxes
 			if bet_amount > 0
-				boxes.each do |match_index, match_boxes|
-					line = lines[match_index.to_i]
 
-					# keys are either 'home' or 'away' to indicate which side was picked
-					match_boxes.each_key do |home_away| 
-						bet_params = {
-							match_time: line["timestamp"],
-							sport: sport,
-							home_team: line["teams"]["home"],
-							away_team: line["teams"]["away"],
-							home_picked: home_away == "home",
-							spread: line["spread"][home_away],
-							odds: line["odds"][home_away],
-							risk_amount: bet_amount
-						}
+				# Count number of checkboxes and multiply by bet_amount to get total risk.
+				# Give an error if user doesn't have enough funds
+				total_risk = 0
+				boxes.each_value do |match_boxes|
+					# each match could have either 1 checkbox checked or 2 checkboxes checked.
+					# I don't know why anyone would bet on both sides of a match, but I guess it's possible.
+					total_risk += match_boxes.size * bet_amount
+				end
 
-						new_bet = Bet.new(bet_params)
-						if new_bet.valid?
-							current_user.bets.push(new_bet)
-							flash[:info] = "Your bets have been placed!"
-						else
-							error_msg = "Some of your bets could not be placed"
+				if total_risk <= current_user.balance
+					boxes.each do |match_index, match_boxes|
+						line = lines[match_index.to_i]
+
+						# keys are either 'home' or 'away' to indicate which side was picked
+						match_boxes.each_key do |home_away| 
+							bet_params = {
+								match_time: line["timestamp"],
+								sport: sport,
+								home_team: line["teams"]["home"],
+								away_team: line["teams"]["away"],
+								home_picked: home_away == "home",
+								spread: line["spread"][home_away],
+								odds: line["odds"][home_away],
+								risk_amount: bet_amount
+							}
+
+							new_bet = Bet.new(bet_params)
+							if new_bet.valid?
+								current_user.bets.push(new_bet)
+								placed_bet_count += 1
+								flash[:info] = "#{placed_bet_count} bets have been placed!"
+							else
+								error_msg = "Some of your bets could not be placed"
+							end
 						end
 					end
+
+					# remove funds from user account.
+					# calculate total here instead of using total_risk just in case some bets couldn't be placed
+					if placed_bet_count > 0
+						current_user.update_attributes(balance: current_user.balance - (placed_bet_count * bet_amount))
+					end
+				else
+					error_msg = "You tried to wager #{ActionController::Base.helpers.number_to_currency(total_risk)} but you don't have enough money!"
 				end
 			else
 				error_msg = "You must wager some money"
