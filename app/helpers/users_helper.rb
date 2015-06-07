3d948036907736bd9fc24c2f3090ab0ded19a561
Mike Chu <mike.c.chu@gmail.com>
2015-06-07 09:37:37 -0700

    add task that will give money to users if they are broke. Task to be run daily at midnight

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+	# reload the wallet of users that have gone broke and have no pending bets
+	def self.reload_wallets
+		users_without_money = User.where(balance: 0)
+
+		users_without_money.each do |user|
+			# if user has no open bets then he is really broke. give him money.
+			if user.bets.where(result: nil).empty?
+				user.update_attributes({balance: 10})
+			end
+		end
+	end
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -4,3 +4,11 @@ task :resolve_bets => :environment do
   BetsHelper.resolve_bets
   puts "done."
 end
+
+
+desc "This task gives money to broke users so they can play again."
+task :reload_wallets => :environment do
+  puts "Reloading wallets of broke users..."
+  UsersHelper.reload_wallets
+  puts "done."
+end
